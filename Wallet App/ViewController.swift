6727e80f2b9915794ab59ccf06048a089e2a1af8
odenysovreaddle <odenysov@readdle.com>
2023-11-15 22:59:54 +0200

    Initial Commit

---
+++ Wallet App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet App
+//
+//  Created by Oleksandr Denysov on 15.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
